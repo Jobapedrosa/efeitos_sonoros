2762c7345e15de9f1d977ed4ee7a0369d418e539
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2023-10-10 20:48:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSomAplausos(){
+document.querySelector("#som_tecla_aplausos").play();
+}
+function tocaSomVaia(){
+    document.querySelector("#som_tecla_vaia").play();
+    }
+function tocaSomRisada(){
+    document.querySelector("#som_tecla_risada").play();
+}    
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomAplausos;
+listaDeTeclas[1].onclick = tocaSomVaia;
+listaDeTeclas[2].onclick = tocaSomRisada;
+
